88b505b44dcbd7efee5d048616332b0da0eb89f4
Jim <wowdacom@gmail.com>
2018-12-19 10:23:10 +0800

    test Mahematical Equations

---
+++ index.js
@@ -0,0 +1,15 @@
+let calculator = {
+    plus: function (num1, num2) {
+        return num1 + num2
+    },
+    minus: function (num1, num2) {
+        return num1 - num2
+    },
+    multiplied: function (num1, num2) {
+        return num1 * num2
+    },
+    divided: function (num1, num2) {
+        return num1 / num2
+    }
+};
+export default calculator
